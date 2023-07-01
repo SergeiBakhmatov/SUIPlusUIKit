60fb4a997162abb5cc5129ee237ad602080be879
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-07-01 12:19:43 +0500

    Initial Commit

---
+++ SUIPlusUIKit/SUIPlusUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIPlusUIKitApp.swift
+//  SUIPlusUIKit
+//
+//  Created by Sergei Bakhmatov on 01.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUIPlusUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
